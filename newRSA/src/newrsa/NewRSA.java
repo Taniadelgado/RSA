10fc857e3e73614da1051af6d14ca6333bc2a383
Taniadelgado <tania119mar@hotmail.com>
2019-08-15 19:46:59 -0500

    v1

---
+++ newRSA/src/newrsa/NewRSA.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newrsa;
+
+/**
+ *
+ * @author TANIA
+ */
+public class NewRSA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          rsa d=new rsa();
+           d.setVisible(true);
+        d.setLocationRelativeTo(null);
+        
+    }
+    
+}
